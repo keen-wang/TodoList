84c25f2ec7ae0ebecf95adddfbe42fb9cc593ab1
Keen <937654393@qq.com>
2019-08-13 04:03:15 +0800

    待办列表信息保存在本地缓存

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,27 @@ Page({
    */
   data: {
     inputValue: '',
-    todolist: [{ content: 'hahahha', finished: false, id: +new Date() }],
+    todolist: [],
     leftCount: 1,
     allSetting: true, // 是否允许全选
     clearSetting: true // 是否允许清楚
+  },
+  onShow () {
+    var todolist = wx.getStorageSync('todolist');
+    if (todolist) {
+      var leftCount = todolist.reduce((result,item) => {
+        return result + (!item.finished?1:0);
+      },0);
+      this.setData({ todolist, leftCount });
+    }
+
+    // ['allSetting', 'clearSetting'].forEach(item=>{
+    //   var setting = wx.getStorageSync(item);
+    //   console.log(item, setting, wx.getStorageSync(item))
+    //   if (typeof setting == 'boolean') {
+    //     this.setData({ [item]: setting });
+    //   }
+    // })
   },
   inputTodo: function (e) {
     // console.log('onItemRemove', e, this.data.inputValue);
@@ -19,15 +36,14 @@ Page({
   addTodo (e) {
     // console.log('addtodo', e);
     if (!this.data.inputValue || !this.data.inputValue.trim()) return;
-    var todos = this.data.todolist;
+    var todolist = this.data.todolist;
     var todo = { content: this.data.inputValue, finished: false, id: +new Date() };
-    todos.push(todo);
-    this.setData({
+    todolist.push(todo);
+    this.save({
       inputValue: '',
-      todolist: todos,
+      todolist,
       leftCount: this.data.leftCount + 1
     });
-    // this.save();
     // getApp().writeHistory(todo, 'create', +new Date());
     // console.log('addtodo', e);
   },
@@ -35,7 +51,7 @@ Page({
     const index = e.currentTarget.dataset.index;
     var todolist = this.data.todolist;
     var removedItem = todolist.splice(index, 1)[0];
-    this.setData({
+    this.save({
       todolist,
       leftCount: this.data.leftCount - (removedItem.finished ? 0 : 1)
     });
@@ -45,7 +61,7 @@ Page({
     const index = e.currentTarget.dataset.index;
     var todolist = this.data.todolist;
     todolist[index].finished = !todolist[index].finished;
-    this.setData({
+    this.save({
       todolist,
       leftCount: this.data.leftCount + (todolist[index].finished ? -1 : 1)
     });
@@ -58,12 +74,17 @@ Page({
       var todolist = this.data.todolist.map(item => ({ ...item, finished: false }));
       var leftCount = this.data.todolist.length;
     }
-    this.setData({ todolist, leftCount})
+    this.save({ todolist, leftCount})
   },
   clearFinished(e) {
     var notFinishedList = this.data.todolist.filter(item=>!item.finished)
-    this.setData({
+    this.save({
       todolist: notFinishedList
     })
-  }
+  },
+  // 更新数据并保存到本地缓存
+  save (data={}) {
+    this.setData(data);
+    wx.setStorageSync('todolist', this.data.todolist);
+  },
 })
